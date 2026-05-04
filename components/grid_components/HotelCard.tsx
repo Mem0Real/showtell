aed517e197df14e8dd74c64fe3f0d9855f702999
Mem0Real <juniourmimo@gmail.com>
2026-05-04 17:02:15 +0300

    folder structure bug fix (test3)

--- components/grid_components/HotelCard.tsx
+++ components/grid_components/HotelCard.tsx
@@ -32,10 +32,12 @@ export const HotelCard: React.FC<HotelCardProps> = ({ hotel }) => {
 
   const [currentHDRI, setCurrentHDRI] = useState(hotel.hdriSrc);
 
+  let directories;
+
   const hdriOptions = [
     { label: 'Exterior', src: hotel.hdriSrc },
-    { label: 'Living Room', src: `/3d/hotels/${hotel.name.replaceAll(' ', '_')}/exterior.jpg` },
-    { label: 'Bedroom', src: `/3d/hotels/${hotel.name.replaceAll(' ', '_')}/exterior.jpg` },
+    { label: 'Living Room', src: `/3d/hotels/${hotel.name.replaceAll(' ', '_').toLowerCase()}/exterior.jpg` },
+    { label: 'Bedroom', src: `/3d/hotels/${hotel.name.replaceAll(' ', '_').toLowerCase()}/exterior.jpg` },
   ];
 
   /* Preload Textures */
